fb5eb47a9dc83bf995420d4e0bf7336302e30533
drkmen <drkmen@rambler.ru>
2015-05-15 17:20:32 +0300

    deploy fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,10 +36,10 @@ set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/
 # set :linked_files, %w{config/database.yml config/secrets.yml}
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 # Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }p
 
 # Default value for keep_releases is 5
 set :keep_releases, 3
