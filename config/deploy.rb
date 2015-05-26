75de5eeee6b5740a2fbfb4c3a76e9a19913f230d
drkmen <drkmen@rambler.ru>
2015-05-26 17:15:36 +0300

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,7 +32,7 @@ set :ssh_options, {
                 }
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', '.env')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/.env')
 # set :linked_files, %w{config/database.yml config/secrets.yml}
 
 # Default value for linked_dirs is []
