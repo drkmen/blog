913a6e33acbf5b49b624beb3b8c877fdfc36537a
drkmen <drkmen@rambler.ru>
2015-06-30 14:49:13 +0300

    fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :set_cookies_for_crawlers
+
   def index
     render 'application/index'
   end
@@ -12,6 +14,13 @@ class ApplicationController < ActionController::Base
     SitemapGenerator::Interpreter.run(:config_file => (SitemapGenerator.app.root + 'config/sitemap.rb'), :verbose => false)
     SitemapGenerator::Sitemap.ping_search_engines
     redirect_to :back
+  end
+
+  def set_cookies_for_crawlers
+    if request.env['HTTP_USER_AGENT'] =~ /Googlebot/
+      cookies[:blog_show_cookie_tips] = false
+      cookies[:blog_show_arrows_tips] = false
+    end
   end
 
   ActiveAdmin::ResourceController.class_eval do
